4666ebfd2199b02795d126b48e1ba08b79f5a77b
Aron Vinicius <aronvinicius15@gmail.com>
2022-03-24 11:14:25 -0300

    Adding Expanded and ListView widgets to render Tarefa class items

--- lib/lista_screen.dart
+++ lib/lista_screen.dart
@@ -1,13 +1,49 @@
 import 'package:flutter/material.dart';
 
 class ListaScreen extends StatelessWidget {
+  Widget getItem() {
+    return new Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: new Icon(
+                    Icons.check_box, 
+                    color: Colors.green
+                  ), 
+                  iconSize: 42.0,
+                  onPressed: () {  },
+                ),
+                new Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget> [
+                    Text("Lavar o carro"),
+                    Text("24/04/08")
+                  ],
+                )
+              ],
+            );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(
           title: new Text("Lista de Tarefas"),
         ),
-        body: Column(),
+        body: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget> [
+            Expanded(
+              child: ListView( 
+                children: <Widget> [
+                  getItem(),
+                  getItem(),
+                  getItem()
+                ],
+              )
+            )
+          ],
+        ),
       );
   }
 }
