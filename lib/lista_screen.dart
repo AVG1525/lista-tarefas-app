3e55f56b0e7fc242f20d2be463453f98949aad0e
Aron Vinicius <aronvinicius15@gmail.com>
2022-03-24 16:44:16 -0300

    Adjusts on icon button component event

--- lib/lista_screen.dart
+++ lib/lista_screen.dart
@@ -1,49 +1,76 @@
 import 'package:flutter/material.dart';
+import 'tarefa.dart';
 
-class ListaScreen extends StatelessWidget {
-  Widget getItem() {
+class ListaScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new ListaScreenState();
+  }
+}
+
+class ListaScreenState extends State<ListaScreen> {
+  List<Tarefa> tarefas = <Tarefa>[];
+  TextEditingController textEditingController = new TextEditingController();
+
+  void AddNewTarefa(String name) {
+    setState(() => tarefas.add(Tarefa(name)));
+
+    textEditingController.clear();
+  }
+
+  void modifyIconButtonState(Tarefa tarefa) {
+    setState(() => tarefa.isCompleted = !tarefa.isCompleted);
+  }
+
+  Widget getItem(Tarefa tarefa) {
     return new Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                IconButton(
-                  icon: new Icon(
-                    Icons.check_box, 
-                    color: Colors.green
-                  ), 
-                  iconSize: 42.0,
-                  onPressed: () {  },
-                ),
-                new Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget> [
-                    Text("Lavar o carro"),
-                    Text("24/04/08")
-                  ],
-                )
-              ],
-            );
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        IconButton(
+          icon: new Icon(
+            tarefa.isCompleted ? Icons.check_box : Icons.check_box_outline_blank,
+            color: Colors.green),
+          iconSize: 42.0,
+          onPressed: () => modifyIconButtonState(tarefa),
+        ),
+        new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              tarefa.name.toString(),
+              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+            ),
+            Text(tarefa.dateTime.toString())
+          ],
+        )
+      ],
+    );  
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Lista de Tarefas"),
-        ),
-        body: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget> [
-            Expanded(
-              child: ListView( 
-                children: <Widget> [
-                  getItem(),
-                  getItem(),
-                  getItem()
-                ],
-              )
-            )
-          ],
-        ),
-      );
+      appBar: new AppBar(
+        title: new Text("Lista de Tarefas"),
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
+              controller: textEditingController,
+              onSubmitted: AddNewTarefa,
+            ),
+          ),
+          Expanded(
+              child: ListView.builder(
+            itemCount: tarefas.length,
+            itemBuilder: (_, index) => getItem(tarefas[index]),
+          ))
+        ],
+      ),
+    );
   }
 }
